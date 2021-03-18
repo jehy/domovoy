ae283a7d1f21b0d961707072413d10911cc00cc1
Jehy <fate@jehy.ru>
2021-03-19 01:25:08 +0300

    seems alive

---
+++ index.js
@@ -0,0 +1,91 @@
+/* eslint-disable no-console,no-await-in-loop */
+
+'use strict';
+
+const TelegramBot = require('node-telegram-bot-api');
+const config = require('config');
+const blueBird = require('bluebird');
+const debug = require('debug')('domovoy');
+const devicesAlive = require('./plugins/devicesAlive');
+
+const plugins = {devicesAlive};
+const pluginsToRun = Object.entries(plugins).filter(([name])=>config[name]);
+
+let messagePool = {};
+
+function makeBot() {
+  const bot = new TelegramBot(config.telegram.token, {polling: true});
+
+  bot.on('polling_error', (error) => {
+    debug('Polling error', error);  // => 'EFATAL'
+    debug(error);
+  });
+
+  bot.on('webhook_error', (error) => {
+    debug('Webhook error', error);  // => 'EPARSE'
+  });
+
+  bot.onText(/\/start/, async (msg) => {
+    debug('start message from user');
+    const chatId = msg.chat.id || msg.from.id;
+    debug(`chatId: ${chatId}`);
+    await bot.sendMessage(chatId, 'Please use /status or wait for info', {
+      reply_markup: JSON.stringify({
+        remove_keyboard: true,
+      }),
+    });
+  });
+  bot.onText(/\/status/, async (msg) => {
+    debug('status message from user');
+    const chatId = msg.chat.id || msg.from.id;
+    debug(`chatId: ${chatId}`);
+    if (config.telegram.chatId !== chatId) {
+      await bot.sendMessage(chatId, 'Sorry, Mario, your princess is another castle!', {
+        reply_markup: JSON.stringify({
+          remove_keyboard: true,
+        }),
+      });
+      return;
+    }
+    const pluginsStatus = pluginsToRun.map(([name, module])=>module.status());
+    await Promise.all(pluginsStatus);
+  });
+  return bot;
+}
+
+function sendMessage(botName, text) {
+  if (!messagePool[botName]) {
+    messagePool[botName] = [];
+  }
+  messagePool[botName].push(text);
+}
+
+async function sendMessageLoop() {
+  const bot = makeBot();
+  while (true) {
+    await blueBird.delay(10000);
+    const text = Object.entries(messagePool).reduce((acc, [pluginName, messages])=>{
+      return acc.concat(`${pluginName}:\n${messages.join('\n')}`);
+    }, []).join('\n');
+    messagePool = {};
+    if (text) {
+      try {
+        await bot.sendMessage(config.telegram.chatId, text);
+      } catch (err) {
+        debug(err);
+      }
+    }
+  }
+}
+
+async function run() {
+  if (!config) {
+    console.log('Config not found!');
+    process.exit(1);
+  }
+  const startPlugins = pluginsToRun.map(([name, module])=>module.run(config[name], (text)=>sendMessage(name, text)));
+  const startJobs = startPlugins.concat(sendMessageLoop());
+  await Promise.all(startJobs);
+}
+
+run();
---
+++ plugins/devicesAlive.js
@@ -0,0 +1,69 @@
+/* eslint-disable no-console,no-await-in-loop */
+
+'use strict';
+
+const ping = require('ping').promise.probe;
+const blueBird = require('bluebird');
+
+let statusData;
+let sendMessage;
+
+function prepareConfig(hostConfig) {
+  const defaults = {
+    timeout: 5000,
+    // interval: 1000 * 60 * 5,
+    interval: 1000,
+    maxDeadCount: 1,
+  };
+  return {...defaults, ...hostConfig};
+}
+
+function setStatusData(current, options) {
+  Object.assign(statusData.find((el)=>el.address === current.address), options);
+}
+
+async function pingHost(hostConfig) {
+  const conf = prepareConfig(hostConfig);
+  let alive;
+  let deadCount = 0;
+  while (true) {
+    let aliveNow = false;
+    try {
+      const res = await ping(conf.address, {timeout: conf.timeout});
+      // console.log(`${conf.name} : `, res);
+      aliveNow = res.alive;
+    } catch (err) {
+      console.log(err);
+      aliveNow = false;
+    }
+    setStatusData(conf, {alive: aliveNow});
+    if (alive === undefined) {
+      alive = aliveNow;
+      sendMessage(`${conf.name} is ${alive ? 'alive' : 'dead'} on startup`);
+    } else if (!aliveNow) {
+      deadCount++; // i suppose we won't have overflow here, huh
+      if (deadCount === conf.maxDeadCount && alive) {
+        sendMessage(`${conf.name} is dead`);
+        alive = false;
+      }
+    } else if (!alive && aliveNow) {
+      sendMessage(`${conf.name} is alive`);
+      deadCount = 0;
+      alive = true;
+    }
+    await blueBird.delay(conf.interval);
+  }
+}
+
+async function run(config, sendMessageFunc) {
+  sendMessage = sendMessageFunc;
+  sendMessage('ping monitor started');
+  statusData = JSON.parse(JSON.stringify(config.hosts));
+  await Promise.all(config.hosts.map((conf) => pingHost(conf)));
+}
+
+async function status() {
+  sendMessage(JSON.stringify(statusData, null, 2));
+}
+
+module.exports = {run, status};
